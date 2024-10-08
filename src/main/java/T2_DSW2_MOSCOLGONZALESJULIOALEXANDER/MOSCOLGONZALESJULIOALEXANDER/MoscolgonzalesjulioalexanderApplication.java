6bd0a3fef52a6fdf789f642fab15fb5e21ceb785
julioMoscol <juliomoscolgonzales@gmail.com>
2024-10-07 21:17:31 -0500

    primer commit

---
+++ src/main/java/T2_DSW2_MOSCOLGONZALESJULIOALEXANDER/MOSCOLGONZALESJULIOALEXANDER/MoscolgonzalesjulioalexanderApplication.java
@@ -0,0 +1,13 @@
+package T2_DSW2_MOSCOLGONZALESJULIOALEXANDER.MOSCOLGONZALESJULIOALEXANDER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoscolgonzalesjulioalexanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoscolgonzalesjulioalexanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/T2_DSW2_MOSCOLGONZALESJULIOALEXANDER/MOSCOLGONZALESJULIOALEXANDER/MoscolgonzalesjulioalexanderApplicationTests.java
@@ -0,0 +1,13 @@
+package T2_DSW2_MOSCOLGONZALESJULIOALEXANDER.MOSCOLGONZALESJULIOALEXANDER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoscolgonzalesjulioalexanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
